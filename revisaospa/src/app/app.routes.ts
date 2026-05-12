10d922eaa8a2eaac4e84a4f8a6d12ded9d417fd8
GabrielNFsesi <gabriel_n_farias@estudante.sesisenai.org.br>
2026-05-11 21:28:07 -0300

    Add Inicio page and update routes/header
    Add a new Inicio component (inicio.ts, inicio.html, inicio.css) and unit test (inicio.spec.ts); register the component in app.routes.ts and import it in app.ts so it is available in the app. Add an 'Inicio' link to the header navigation. Also update several image sources in the angular and tailwind page templates to new URLs.

--- revisaospa/src/app/app.routes.ts
+++ revisaospa/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Angular } from './page/angular/angular';
 import { Tailwind } from './page/tailwind/tailwind';
 import { Impactos } from './page/impactos/impactos';
+import { Inicio } from './inicio/inicio';
 
 export const routes: Routes = [
     {path:'angular', component:Angular},
    {path:'tailwind', component:Tailwind},
     {path:'impactos', component:Impactos},
+    {path:'inicio', component:Inicio},
 ];
  
         
--- revisaospa/src/app/app.ts
+++ revisaospa/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './component/header/header';
 import { Footer } from './component/footer/footer';
+import { Inicio } from './inicio/inicio';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer],
+  imports: [RouterOutlet,Header,Footer,Inicio],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
